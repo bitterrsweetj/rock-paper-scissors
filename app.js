e5b88387d54216e01e3786f2ed0f965c85181d69
Elvira <esinyanskaya@gmail.com>
2022-12-29 22:50:55 +0200

    Adding playGame function

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
-function getComputerChoice() {
-  const choices = [
-    "Rock", 
-    "Paper", 
-    "Scissors"
-  ];
-  let randomChoice = choices[Math.floor(Math.random()*choices.length)];
-  console.log(randomChoice);
+let computerSelection;
+let playerSelection;
+let score = 0;
+
+function getPlayerChoice() {
+  return prompt("choose rock, paper, scissors");
 }
 
-getComputerChoice();
+function getComputerChoice() {
+  const choices = [
+    "rock",
+    "paper",
+    "scissors"
+  ];
+  const randomIndex = [Math.floor(Math.random() * choices.length)];
+  return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice();
+  playerSelection = getPlayerChoice();
+
+  console.log("player chose", playerSelection);
+  console.log("computer chose", computerSelection);
+
+  if (playerSelection == computerSelection) {
+    console.log("draw");
+  } else {
+    if (playerSelection == "rock") {
+      if (computerSelection == "scissors") {
+        console.log("win");
+        score++;
+      } else if (computerSelection == "paper") {
+        console.log("lose");
+        score--;
+      }
+    }
+    else if (playerSelection == "scissors") {
+      if (computerSelection == "paper") {
+        console.log("win");
+        score++;
+      } else if (computerSelection == "rock") {
+        console.log("lose");
+        score--;
+      }
+    }
+    else if (playerSelection == "paper") {
+      if (computerSelection == "rock") {
+        console.log("win");
+        score++;
+      } else if (computerSelection == "scissors") {
+        console.log("lose");
+        score--;
+      }
+    }
+  }
+  console.log("overall score", score);
+  return score;
+}
+
+  function playGame() {
+    for (let i = 0; i < 5; i++) {
+      playRound(playerSelection, computerSelection)
+    }
+  }
+
+  playGame();
